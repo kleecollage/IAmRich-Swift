2e5c91e035833492917307862f34bc5698b6b681
kleeCollage <klee.collage@gmail.com>
2025-12-25 17:49:57 -0600

    Initial Commit

---
+++ I am RichTests/I_am_RichTests.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_RichTests.swift
+//  I am RichTests
+//
+//  Created by Antonio Hernández Santander on 25/12/25.
+//
+
+import Testing
+@testable import I_am_Rich
+
+struct I_am_RichTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
