a469568ea925257abdab4987dcc28d680f9be657
kleeCollage <klee.collage@gmail.com>
2025-12-25 22:24:14 -0600

    Initial Commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Antonio Hernández Santander on 25/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
